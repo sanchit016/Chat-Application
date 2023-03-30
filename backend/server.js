af931af833fe72849cb3fb26908c267aa89e19a2
Sanchit96 <ue203096.sanchit.cse@gmail.com>
2023-03-30 15:26:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,19 +32,18 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   console.log("here");
   app.use(express.static(path.join(__dirname1, "build")));
+  console.log("here10");
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "build", "index.html"))
   );
+  console.log("here11");
 } else {
   // console.log("here2");
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
-
-
-console.log("here2");
 
 // --------------------------deployment------------------------------
 
@@ -54,55 +53,48 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
-console.log("here3");
-
 const server = app.listen(
   PORT,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
-// const io = require("socket.io")(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "http://localhost:3001",
-//     // credentials: true,
-//   },
-// });
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3001",
+    // credentials: true,
+  },
+});
 
-console.log("here4");
+io.on("connection", (socket) => {
+  console.log("here6");
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
 
-// io.on("connection", (socket) => {
-//   console.log("here6");
-//   console.log("Connected to socket.io");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData._id);
-//     socket.emit("connected");
-//   });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("User Joined Room: " + room);
-//   });
-//   socket.on("typing", (room) => socket.in(room).emit("typing"));
-//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
 
-//   socket.on("new message", (newMessageRecieved) => {
-//     var chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("chat.users not defined");
 
-//     if (!chat.users) return console.log("chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
 
-//     chat.users.forEach((user) => {
-//       if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
 
-//       socket.in(user._id).emit("message recieved", newMessageRecieved);
-//     });
-//   });
-
-//   socket.off("setup", () => {
-//     console.log("USER DISCONNECTED");
-//     socket.leave(userData._id);
-//   });
-// });
-
-
-console.log("here5");
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});

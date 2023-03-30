a4fcbea462e6e1eef79b386c7160a77bc266b2de
Sanchit96 <ue203096.sanchit.cse@gmail.com>
2023-03-30 15:36:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,20 +17,20 @@ app.use(express.json()); // to accept json data
 //   res.send("API Running!");
 // });
 
-console.log("here1");
+try{
+  console.log("here1");
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
+  app.use("/api/user", userRoutes);
+  app.use("/api/chat", chatRoutes);
+  app.use("/api/message", messageRoutes);
 
-// --------------------------deployment------------------------------
+  // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+  const __dirname1 = path.resolve();
 
-// console.log(process.env.NODE_ENV, process.env.NODE_ENV === "production");
+  // console.log(process.env.NODE_ENV, process.env.NODE_ENV === "production");
 
-if (process.env.NODE_ENV === "production") {
-  try{
+  if (process.env.NODE_ENV === "production") {
     console.log("here");
     app.use(express.static(path.join(__dirname1, "build")));
     console.log("here10");
@@ -39,67 +39,67 @@ if (process.env.NODE_ENV === "production") {
       res.sendFile(path.resolve(__dirname1, "build", "index.html"))
     );
     console.log("here11");
+  } else {
+    // console.log("here2");
+    app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
   }
-  catch(err){
-    console.log(err);
-  }
-} else {
-  // console.log("here2");
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 
-// --------------------------deployment------------------------------
+  // --------------------------deployment------------------------------
 
-// Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
+  // Error Handling middlewares
+  app.use(notFound);
+  app.use(errorHandler);
 
-const PORT = process.env.PORT;
+  const PORT = process.env.PORT;
 
-const server = app.listen(
-  PORT,
-  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
-);
+  const server = app.listen(
+    PORT,
+    console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+  );
 
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3001",
-    // credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("here6");
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
+  const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3001",
+      // credentials: true,
+    },
   });
 
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  io.on("connection", (socket) => {
+    console.log("here6");
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-    if (!chat.users) return console.log("chat.users not defined");
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
 
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+      if (!chat.users) return console.log("chat.users not defined");
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
     });
   });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
+}
+catch(err){
+  console.log(err);
+}

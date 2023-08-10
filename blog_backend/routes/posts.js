6ce13f4e7d3695d424562b93d34bcc2e5ef5a507
Sanchit <98282778+sanchit016@users.noreply.github.com>
2023-08-10 12:36:52 +0530

    removed unimplemented features

--- blog_backend/routes/posts.js
+++ blog_backend/routes/posts.js
@@ -70,18 +70,12 @@ router.get("/:id", async (req, res) => {
 //GET ALL POSTS
 router.get("/", async (req, res) => {
   const username = req.query.user;
-  const catName = req.query.cat;
   try {
     let posts;
     if (username) {
       posts = await Post.find({ username });
-    } else if (catName) {
-      posts = await Post.find({
-        categories: {
-          $in: [catName],
-        },
-      });
-    } else {
+    }
+    else {
       posts = await Post.find();
     }
     temp={"data": posts};

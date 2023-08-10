8bd6294b1c4cb5ed14dc850db76c01871f167a50
Sanchit <98282778+sanchit016@users.noreply.github.com>
2023-08-10 12:35:42 +0530

    removed unimplemented features

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
@@ -47,7 +46,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
 
 app.listen("3001", () => {
   console.log("Backend is running.");
